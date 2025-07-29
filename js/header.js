75f69ce3c5750aaa623c100a1f1b189ea080e0f6
reliruleru00 <93340772+reliruleru00@users.noreply.github.com>
2025-07-29 23:20:49 +0900

    改修版

--- js/header.js
+++ js/header.js
@@ -1,10 +1,6 @@
 // リンクリスト
 const link_list = [
-    {url: ".", title: "ダメージ計算ツール"},
-    {url: "spod_sim.html", title: "SP/ODシミュレータ"},
-    {url: "style_checker.html", title: "スタイル所持率チェッカー"},
-    {url: "arts_list.html", title: "アーツデッキ画像生成"},
-    {url: "chara_mgmt.html", title: "キャラ管理ツール"},
+    {url: "https://www.hbr-tool.com/", title: "ダメージ計算式改修後"},
 ]
 
 $(function(){ 
@@ -12,7 +8,6 @@ $(function(){
     link_list.forEach(element => {
         ui.append($('<li>').append($('<a>', {class: 'header_link', href: element.url, text: element.title})));
     });
-    ui.append($('<li>').append($('<a>', {class: 'header_link', href: "siteinfo.html", text: "このサイトについて"})));
     let headerHTML = $('<div>', {class: 'hamburger'})
         .append($('<img>', {class: 'logo', src: 'img/title_log.png'}))
         .append($('<p>', {class: 'btn-gNav'})
